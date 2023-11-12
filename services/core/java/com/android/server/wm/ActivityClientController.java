c93a9e9b11436f83d2b5cc06de19fda6b5523168
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-12 08:15:47 +0800

    services: Implement activity boost optimization
    neobuddy89:
    * Declare services in constructor.
    * Add mIsBoosted to ensure we dont cancel boost not enabled by us.
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/ActivityClientController.java
+++ services/core/java/com/android/server/wm/ActivityClientController.java
@@ -507,6 +507,7 @@ class ActivityClientController extends IActivityClientController.Stub {
             final long origId = Binder.clearCallingIdentity();
             Trace.traceBegin(TRACE_TAG_WINDOW_MANAGER, "finishActivity");
             try {
+                r.setActivityBoost(false);
                 final boolean res;
                 mTaskSupervisor.getBackgroundActivityLaunchController()
                         .onActivityRequestedFinishing(r);
