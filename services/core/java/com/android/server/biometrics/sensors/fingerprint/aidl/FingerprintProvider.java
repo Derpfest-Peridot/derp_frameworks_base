d6147e623aa1ba319bbd88d54fd4b029d1fd3d76
kenway214 <kenway214@outlook.com>
2025-01-09 03:40:31 +0530

    Revert "Optionally use fingerprint virtual hal eith BiometricTestSession"
    This reverts commit fbef2e303def7c35a7e75503e315a8044075e772.

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
@@ -914,13 +914,7 @@ public class FingerprintProvider implements IBinder.DeathRecipient, ServiceProvi
     }
 
     void setTestHalEnabled(boolean enabled) {
-        final boolean changed = enabled != mTestHalEnabled;
         mTestHalEnabled = enabled;
-        Slog.i(getTag(), "setTestHalEnabled(): useVhalForTesting=" + Flags.useVhalForTesting()
-                + "mTestHalEnabled=" + mTestHalEnabled + " changed=" + changed);
-        if (changed && useVhalForTesting()) {
-            getHalInstance();
-        }
     }
 
     public boolean getTestHalEnabled() {
@@ -1012,7 +1006,7 @@ public class FingerprintProvider implements IBinder.DeathRecipient, ServiceProvi
         if (mVhal == null && useVhalForTesting()) {
             mVhal = IVirtualHal.Stub.asInterface(mDaemon.asBinder().getExtension());
             if (mVhal == null) {
-                Slog.e(getTag(), "Unable to get fingerprint virtualhal interface");
+                Slog.e(getTag(), "Unable to get virtual hal interface");
             }
         }
 
